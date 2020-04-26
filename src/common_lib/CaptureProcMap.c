6fae9eb581ea98c5bb08a166b5857bf7d045555d
Benjamin Welton <welton@cs.wisc.edu>
2020-04-26 00:50:31 -0500

    SMall bug fix

--- src/common_lib/CaptureProcMap.c
+++ src/common_lib/CaptureProcMap.c
@@ -14,15 +14,15 @@ void CaptureProcMap_Write(char * filename) {
 		fprintf(stderr, "%s %d\n", "COULD NOT OPEN PROCMAP FOR: ", getpid());
 		return;
 	}
-
+	c = fgetc(readFile);
 	while (c != EOF && c != 0xff && feof(readFile) == 0){
-		c = fgetc(readFile);
 		buffer[copySize] = c;
 		copySize++;
 		if (copySize == 4096){
 			fwrite(buffer,1,4096,fout);
 			copySize = 0;
 		}
+		c = fgetc(readFile);
 	}
 	if (copySize != 0){
 		fwrite(buffer,1,copySize,fout);
